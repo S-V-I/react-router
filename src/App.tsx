a3a131bb159877a25784399a756862ef445ba872
S-V-I <ambitious.sv@gmail.com>
2024-08-26 19:18:21 +0300

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext } from "react";
+import "./App.css";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+
+import { Link } from "react-router-dom";
+
+const Home: React.FC = () => {
+  return (
+    <div className="App-header">
+      <div>
+        <Link className="App-link" to="/dashboard">
+          Dashboard
+        </Link>
+      </div>
+      <div>
+        <Link className="App-link" to="/login">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const Login: React.FC = () => {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (auth) {
+      auth.login(true);
+      navigate("/dashboard");
+    }
+  };
+
+  return (
+    <div onClick={handleClick} className="App-header">
+      Login
+    </div>
+  );
+};
+
+const Dashboard: React.FC = () => {
+  return <div className="App-header">Dashboard Page</div>;
+};
+
+const Error: React.FC = () => {
+  return <div className="App-header">Error Page</div>;
+};
+
+type ContextType = {
+  isLogged: boolean;
+  login: (status: boolean) => void;
+};
+
+const AuthContext = createContext<ContextType | null>(null);
+
+const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const auth = useContext(AuthContext);
+
+  if (!auth) return <Error />;
+
+  return auth.isLogged ? <>{children}</> : <Navigate to="/login" replace />;
+};
+
+const Profile: React.FC = () => {
+  const { profileId, menuId } = useParams();
+
+  React.useEffect(() => {
+    alert(`Завантаження даних для ID: ${profileId}`);
+  }, [profileId]);
+
+  return (
+    <div className="App-header">
+      Profile Page ID: {profileId} Menu: {menuId}
+    </div>
+  );
+};
 
 function App() {
+  const [isLogged, login] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ isLogged, login }}>
+      <BrowserRouter>
+        <Routes>
+          <Route index Component={Home} />
+          <Route path="/login/*" Component={Login} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile/:profileId/menu/:menuId"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/*" Component={Error} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
